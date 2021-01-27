d573de7de923002605baaf820359d48122d32014
weiwei163@foxmail.com <ylqchengwei5566~>
2021-01-27 16:58:25 +0800

    add: 修正module配置

--- src/think/module/Fields.php
+++ src/think/module/Fields.php
@@ -38,7 +38,7 @@ class Fields
                 foreach ($config as $item) {
                     $fieldModuleMapData[$moduleKey]['fixed'][$item['field']] = $item;
 
-                    if($item['field'] === "tenant_id"){
+                    if ($item['field'] === "tenant_id") {
                         // 包含租户id
                         Module::setTenantIdModules($moduleKey);
                     }
@@ -50,5 +50,230 @@ class Fields
         }
 
         return $fieldModuleMapData;
+    }
+
+    /**
+     * 判断当前字段是否为必须
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldRequire($field)
+    {
+        if (in_array($field, ['name', 'phone', 'password', 'value', 'ptype', 'status', 'man_hour', 'type', 'attribute_value'])) {
+            return 'yes';
+        }
+
+        if (strpos($field, '_id')) {
+            return 'yes';
+        }
+
+        return 'no';
+    }
+
+    /**
+     * 判断当前字段是否能编辑
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldEdit($field)
+    {
+        if (in_array($field, ['id', 'uuid', 'created_by', 'created', 'json', 'is_horizontal'])) {
+            return 'deny';
+        }
+
+        if (strpos($field, '_id')) {
+            return 'deny';
+        }
+
+        return 'allow';
+    }
+
+    /**
+     * 判断当前字段是否能显示
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldShow($field)
+    {
+        if (in_array($field, ['json', 'password'])) {
+            return 'no';
+        }
+
+        return 'yes';
+    }
+
+
+    /**
+     * 判断当前字段是否能排序
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldSort($field)
+    {
+        if (in_array($field, ['name', 'code', 'attribute_id', 'start_time', 'end_time', 'type', 'created_by', 'created', 'project_id', 'category_id', 'step_category_id'])) {
+            return 'allow';
+        }
+
+
+        return 'deny';
+    }
+
+
+    /**
+     * 判断当前字段是否能分组
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldGroup($field)
+    {
+        if (strpos($field, '_id')) {
+            return 'allow';
+        }
+
+        return 'deny';
+    }
+
+    /**
+     * 判断当前字段是否能过滤
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldFilter($field)
+    {
+        if (in_array($field, ['id', 'uuid', 'json', 'config', 'param', 'admin_password', 'node_config'])) {
+            return 'deny';
+        }
+
+        return 'allow';
+    }
+
+    /**
+     * 判断当前字段是否能过滤
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldPrimaryKey($field)
+    {
+        if ($field === 'id') {
+            return 'yes';
+        }
+
+        return 'no';
+    }
+
+    /**
+     * 判断当前字段是否能过滤
+     * @param $field
+     * @return string
+     */
+    public static function checkFieldForeignKey($field)
+    {
+        if (strpos($field, '_id')) {
+            return 'yes';
+        }
+
+        return 'no';
+    }
+
+    /**
+     * 获取固定字段的编辑器类型
+     * @param $field
+     * @param $type
+     * @return string
+     */
+    public static function getFixedFieldEditor($field, $type)
+    {
+        if (
+            strpos($field, '_id')
+            || strpos($field, 'enum')
+            || in_array($field, ['created_by', 'is_horizontal', 'resolution', 'delivery_platform', 'ptype', 'assignee', 'executor'])
+        ) {
+            return 'select';
+        }
+
+        if (
+            strpos($field, '_time')
+            || in_array($field, ['created'])
+        ) {
+            return 'date';
+        }
+
+        if (in_array($field, ['ssl', 'tls'])) {
+            return 'switch';
+        }
+
+        if (
+            strpos($type, 'varchar')
+            || strpos($type, 'char')
+            || strpos($type, 'int')
+        ) {
+            return 'input';
+        }
+
+        if (in_array($type, ['text', 'longtext'])) {
+            return 'text_area';
+        }
+
+
+        return 'none';
+    }
+
+    /**
+     * 生成字段配置
+     * @param $modelName
+     * @param $realName
+     * @param $moduleID
+     * @param $field
+     * @param $param
+     * @return array
+     */
+    public static function generateFieldConfig($modelName, $realName, $moduleID, $field, $param)
+    {
+        // 默认 id 字段就是主键
+        $isPrimary = $field === 'id' ? "yes" : "no";
+
+        // 默认 带_id的参数都属于外键
+        $isForeign = strpos($field, '_id') === false ? "no" : "yes";
+
+        $fieldConfig = [
+            "id" => 0, // 字段id, 固定字段是0，自定义字段是注册的id值
+            "field" => $field, // 字段名
+            "type" => $param['type'], //字段类型
+            "field_type" => "built_in", //字段类型 built_in：固定字段，custom：自定义字段
+            "disabled" => "no", // 是否禁用（yes, no）
+            "require" => self::checkFieldRequire($field), // 是否必须（yes, no）
+            "table" => $modelName, // 所属表名
+            "module_code" => $realName, // 所属模块名
+            "module_id" => $moduleID, // 模块id
+            "lang" => strtoupper($field), // 语言包KEY
+            "editor" => self::getFixedFieldEditor($field, $param['type']), // 编辑器类型
+            "edit" => self::checkFieldEdit($field), // 是否可以编辑（allow, deny）
+            "show" => self::checkFieldShow($field), // 是否在前台显示 （yes, no）
+            "sort" => self::checkFieldSort($field), // 是否可以排序（allow, deny）
+            "group" => self::checkFieldGroup($field), // 是否可以分组
+            "group_name" => "", // 分组显示名称
+            "filter" => self::checkFieldFilter($field), // 是否可以过滤（allow, deny）
+            "multiple" => "no", // 是否可以多选（yes, no）
+            "format" => [], // 格式化配置
+            "validate" => "", // 验证方法
+            "mask" => "", // 掩码配置
+            "is_primary_key" => $isPrimary, // 是否是主键（yes, no）
+            "is_foreign_key" => $isForeign, // 是否是外键（yes, no）
+            "placeholder" => "no", // 输入框占位文本 （yes, no）
+            "show_word_limit" => "no", // 是否显示输入字数统计 （yes, no）
+            "autocomplete" => "no", // 是否自动补全 （yes, no）
+            "value_icon" => "", // 值图标
+            "label_icon" => "", // 文本图标
+            "label_width" => 0, // 文本宽度
+            "value_width" => 0, //  值宽度
+            "is_label" => "no",  //  是否显示文本 （yes, no）
+            "default_value" => "", // 默认值
+            "data_source" => [ // 数据源
+                "type" => "fixed", // 数据源类型，fixed 固定 , dynamic 动态
+                "data" => [] // 数据源，静态直接配置，动态是一个字符串标识
+            ]
+        ];
+
+        return $fieldConfig;
     }
 }
--- src/think/module/Module.php
+++ src/think/module/Module.php
@@ -30,13 +30,109 @@ class Module
      */
     public static function init()
     {
-        if(empty(self::$moduleDictData)){
+        if (empty(self::$moduleDictData)) {
             self::getModuleData();
             self::generateModuleFieldCache(self::$moduleDictData['module_index_by_id']);
-
         }
     }
 
+    /**
+     * _ 名称转驼峰
+     * @param $unCamelizeWords
+     * @param string $separator
+     * @return mixed
+     */
+    public static function camelize($unCamelizeWords, $separator = '_')
+    {
+        $unCamelizeWords = $separator . str_replace($separator, " ", strtolower($unCamelizeWords));
+        return str_replace(" ", "", ucwords(ltrim($unCamelizeWords, $separator)));
+    }
+
+    /**
+     * 获取所有模块数据
+     * @param $tempDb
+     * @return array
+     */
+    public static function getAllModuleData($tempDb)
+    {
+        $result = $tempDb->query("select * from module");
+        $info = array();
+        foreach ($result as $key => $val) {
+            $info[$val['code']] = $val;
+        }
+        return $info;
+    }
+
+    /**
+     * 修正数据结构
+     * @param $fixedModule
+     * @param $tempDb
+     */
+    public static function fixedModuleConfig($fixedModule, $tempDb)
+    {
+
+        // 获取所有存在的表
+        $moduleMap = self::getAllModuleData($tempDb);
+
+        foreach ($fixedModule as $fixedItem) {
+            if (!array_key_exists($fixedItem, $moduleMap)) {
+                $tables[] = [
+                    'type' => 'fixed',
+                    'name' => $fixedItem
+                ];
+            }
+        }
+
+        $result = $tempDb->query("select max(id) as max_id from module");
+        $insertId = !empty($result[0]['max_id']) ? $result[0]['max_id'] + 1 : 1;
+
+        foreach ($tables as $tableItem) {
+
+            if ($tableItem["name"] !== "phinxlog") {
+
+                // 组装注册固定模块数据
+                $modelName = $tableItem["type"] === "entity" ? 'Entity' : self::camelize($tableItem["name"]);
+
+                $moduleRows[] = [
+                    'type' => $tableItem["type"],
+                    'active' => 'yes',
+                    'name' => self::camelize($tableItem["name"]),
+                    'code' => $tableItem["name"],
+                    'icon' => '',
+                    'uuid' => Webpatser\Uuid\Uuid::generate()->string
+                ];
+
+                // 组装当前模块字段配置数据
+                $tableName = $tableItem["type"] === "entity" ? 'entity' : $tableItem["name"];
+                $currentTableFields = $tempDb->getFields($tableName);
+
+                $tempConfig = [];
+                foreach ($currentTableFields as $field => $param) {
+                    $tempConfig[] = Fields::generateFieldConfig($modelName, $tableItem["name"], $insertId, $field, $param);
+                }
+
+                $fieldsRows[] = [
+                    'table' => $tableName,
+                    'module_id' => $insertId,
+                    'config' => json_encode($tempConfig),
+                    'uuid' => Webpatser\Uuid\Uuid::generate()->string
+                ];
+
+                $insertId++;
+            }
+        }
+
+        // 写入模块数据表
+        if (!empty($moduleRows)) {
+            $tempDb->insertAll($moduleRows, ['table' => 'module', 'model' => 'module']);
+        }
+
+
+        // 写入字段数据表
+        if (!empty($fieldsRows)) {
+            $tempDb->insertAll($fieldsRows, ['table' => 'field', 'model' => 'module']);
+        }
+    }
 
     /**
      * 检测数据结构，使用本框架必须包含
@@ -48,23 +144,27 @@ class Module
         $options = Db::parseConfig();
         $class = 'think\\db\\driver\\' . ucwords(strtolower($options['type']));
         if (class_exists($class)) {
-            $tempDb =  new $class($options);
+            $tempDb = new $class($options);
         } else {
             // 类没有定义
-            throw new \RuntimeException('Class is not defined '. $class);
+            throw new \RuntimeException('Class is not defined ' . $class);
         }
 
         $tables = $tempDb->getTables();
-        unset($tempDb);
-
-        if(!empty($tables) && is_array($tables)){
-            foreach (self::$requireModules as $requireModule){
-                if(!in_array($requireModule, $tables)){
-                    throw new \RuntimeException('Data structure error. Need module '. join(',', self::$requireModules));
+        if (!empty($tables) && is_array($tables)) {
+            foreach (self::$requireModules as $requireModule) {
+                if (!in_array($requireModule, $tables)) {
+                    throw new \RuntimeException('Data structure error. Need module ' . join(',', self::$requireModules));
                 }
             }
-        }else{
-            throw new \RuntimeException('Data structure error. Need module '. join(',', self::$requireModules));
+
+            // 修正module配置
+            self::fixedModuleConfig($tables, $tempDb);
+
+            // 销毁对象
+            unset($tempDb);
+        } else {
+            throw new \RuntimeException('Data structure error. Need module ' . join(',', self::$requireModules));
         }
     }
 
