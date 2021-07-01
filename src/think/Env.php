c6a4b012774b68cf5197130c22632a236e2a5ac8
weiwei163@foxmail.com <ylqchengwei5566~>
2021-07-01 16:56:13 +0800

    enhanced: Env.php 改造成强类型

--- src/think/Env.php
+++ src/think/Env.php
@@ -1,12 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// | The teamones framework runs on the workerman high performance framework
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2017 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
+// | Reviser: weijer <weiwei163@foxmail.com>
 // +----------------------------------------------------------------------
 
 namespace think;
@@ -15,11 +19,11 @@ class Env
 {
     /**
      * 获取环境变量值
-     * @param string    $name 环境变量名（支持二级 .号分割）
-     * @param string    $default  默认值
-     * @return mixed
+     * @param string $name 环境变量名（支持二级 .号分割）
+     * @param ?string $default 默认值
+     * @return array|mixed|string|null
      */
-    public static function get($name, $default = null)
+    public static function get(string $name, $default = null)
     {
         $result = getenv(ENV_PREFIX . strtoupper(str_replace('.', '_', $name)));
         if (false !== $result) {
